0515a1526fe012cd75ad4d0613c5521b4df9e295
Nayef Muhiar <nayef@buttercloud.com>
2013-07-02 11:18:34 +0300

    Initial commit of demo_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'b61363b14a7c46f97bbfe021bb8e984babe3f7c3b40707c27ef4d66b7f43fa7b8726a33865b56ec6076115879393722549afd2998132d222ce87a0ed46ad9313'
